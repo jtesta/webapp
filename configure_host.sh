cc7f65488034fdaf3c533f5b2357fee3ba2231d0
Joe Testa <jtesta@positronsecurity.com>
2025-05-05 15:06:55 -0400

    Updated systemd user.

--- configure_host.sh
+++ configure_host.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 
-echo -e "Updating packages...\n"
+echo -e "Creating webapp user...\n"
+useradd -d /opt/webapp -s /usr/sbin/nologin webapp
+
+echo -e "\nUpdating packages...\n"
 apt update
 
 echo -e "\nInstalling package prerequisites...\n"
-apt install -y python3-flask nginx libnginx-mod-http-modsecurity libmodsecurity3t64 python3-gunicorn python3-jwt python3-flaskext.wtf
+apt install -y gunicorn nginx python3-flask libnginx-mod-http-modsecurity libmodsecurity3t64 python3-gunicorn python3-jwt python3-flaskext.wtf
 
 echo -e "\nConfiguring nginx...\n"
 cp nginx-site.conf /etc/nginx/sites-available/default
@@ -19,7 +22,7 @@ echo -e "\nConfiguring registering webapp with systemd...\n"
 # Reset the database directory, and ensure it is writeable by the gunicorn process.
 rm -rf /opt/webapp/instance
 mkdir -m 0700 /opt/webapp/instance
-chown nobody:nogroup /opt/webapp/instance
+chown webapp:webapp /opt/webapp/instance
 
 cp flask-webapp.service /etc/systemd/system/
 systemctl daemon-reload
